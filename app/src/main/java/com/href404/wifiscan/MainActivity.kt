2b2bc4961e6c8a88d77516cd990fd94758aef710
Pierre Julien <pierrejulienpro@gmail.com>
2022-02-27 20:15:42 +0100

    Ajout du viewBinding dans l'application

--- app/src/main/java/com/href404/wifiscan/MainActivity.kt
+++ app/src/main/java/com/href404/wifiscan/MainActivity.kt
@@ -6,15 +6,19 @@ import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.href404.wifiscan.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity(), NetworkListener {
 
+    private lateinit var binding: MainActivityBinding
     private val networkService = NetworkService.instance
     private val wifiReceiver = WifiBroadcastReceveiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         networkService.subscribe(this)
         registerReceiver(wifiReceiver, IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION))
@@ -32,10 +36,10 @@ class MainActivity : AppCompatActivity(), NetworkListener {
     override fun onScanResult(networks: List<ScanResult>) {
         val network = networks.first()
 
-        findViewById<TextView>(R.id.network_counter).text = networks.size.toString()
-        findViewById<TextView>(R.id.ssid).text = network.SSID
-        findViewById<TextView>(R.id.frequency).text = network.frequency.toString()
-        findViewById<TextView>(R.id.level).text = network.level.toString()
+        binding.networkCounter.text = networks.size.toString()
+        binding.ssid.text = network.SSID
+        binding.frequency.text = network.frequency.toString()
+        binding.level.text = network.level.toString()
     }
 }
 
