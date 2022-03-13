896dec1a95daad27994855f958d38b20bc189b35
Pierre Julien <pierrejulienpro@gmail.com>
2022-03-13 11:02:51 +0100

    Refactoring test ui anciennement dans l'activité pour être fonctionnel sur le fragment d'accueil

--- app/src/androidTest/java/com/href404/wifiscan/HomeFragmentTest.kt
+++ app/src/androidTest/java/com/href404/wifiscan/HomeFragmentTest.kt
@@ -1,6 +1,6 @@
 package com.href404.wifiscan
 
-import androidx.test.core.app.ActivityScenario
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -12,17 +12,17 @@ import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
-class MainActivityTest {
+class HomeFragmentTest {
 
     @Test
     fun displayWifiInformations() {
-        val activityScenario = ActivityScenario.launch(MainActivity::class.java)
+        val scenario = launchFragmentInContainer<HomeFragment>()
 
         onView(withId(R.id.network_counter)).check(matches(isDisplayed()))
         onView(withId(R.id.ssid)).check(matches(isDisplayed()))
         onView(withId(R.id.frequency)).check(matches(isDisplayed()))
         onView(withId(R.id.level)).check(matches(isDisplayed()))
 
-        activityScenario.close()
+        scenario.close()
     }
 }
